3fa539ca34cb6ebb2eb57c56b1c383b935f6c717
Nyx <lilizhao107513@gmail.com>
2024-01-28 17:50:55 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,6 +5,8 @@ class X:
     def __repr__(self):
         return "X"
     
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -12,6 +14,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +25,10 @@ class Add:
     
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+
 
 class Mul:
     def __init__(self, p1, p2):
@@ -49,6 +58,9 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -57,6 +69,9 @@ class Sub:
     
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
     
 class Div:
     def __init__(self, p1, p2):
@@ -96,11 +111,12 @@ class Div:
         
         return repr(self.p1) + " / " + repr(self.p2)
     
+    def evaluate(self, x):
+        if self.p2.evaluate(x) == 0:
+            raise ValueError("Division by Zero")
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
-#poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-#poly = Add(X(), Mul(X(), X()))
-#poly2 = Div(X(), Int(0))
-#poly = Div(Int(5), X())
-poly =Div(Div(X(), X()), X())
+#poly = Div(Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1))))), X())
+poly = Div(Mul(Add(X(), Int(2)), Mul(Sub(Mul(X(), X()), Mul(Int(3), X())), Int(4))), Sub(X(), Int(1)))
 print(poly)
-#print(poly2)
+print(poly.evaluate(3))
